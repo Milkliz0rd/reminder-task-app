1656fa5ed342f9741b0aebf489b3306474f53f62
Alexandre Morozoff <alexandre.morozoff@osol.co>
2026-06-05 09:58:41 +0200

    fix(cron) - forget to put async/await on try/catch

--- backend/src/jobs/emailJob.ts
+++ backend/src/jobs/emailJob.ts
@@ -1,9 +1,9 @@
 import cron from "node-cron";
 import { sendDailyReminder } from "../services/emailService";
 
-cron.schedule(" 0 8 * * *", () => {
+cron.schedule(" 0 8 * * *", async () => {
   try {
-    sendDailyReminder();
+    await sendDailyReminder();
   } catch (error) {
     console.error("Email job failed: ", error);
   }
